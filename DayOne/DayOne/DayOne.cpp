e9084182a8e7cf56328d0fefe8bb4c669bb42fa0
Adam Watson (He/Him) <awatson@scottlogic.com>
2022-12-01 12:59:35 +0000

    day one part one

---
+++ DayOne/DayOne/DayOne.cpp
@@ -0,0 +1,40 @@
+﻿// DayOne.cpp : Day One of AOC.
+//
+
+#include "DayOne.h"
+
+using namespace std;
+
+int main()
+{
+	std::ifstream input("input.txt");
+
+	int previousHighestCal = 0;
+
+	if (input.is_open()) {
+		while (input) {
+			std::string line;
+			std::getline(input, line);
+
+			int totalCal = 0;
+			while (line != "") {
+				int cal = stoi(line);
+				totalCal += cal;
+
+				std::getline(input, line);
+			}
+
+			if (totalCal > previousHighestCal) {
+				previousHighestCal = totalCal;
+			}
+		}
+
+		std::cout << "Highest cal: " << previousHighestCal;
+	}
+	else {
+		std::cout << "Failed to open file.";
+		return -1;
+	}
+
+	return 0;
+}

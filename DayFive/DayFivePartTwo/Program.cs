9421c79b968ea79c62ec8b218cc5faed41ac6df1
Adam Watson (He/Him) <awatson@scottlogic.com>
2022-12-05 17:16:00 +0000

    Day five part two

---
+++ DayFive/DayFivePartTwo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Text.RegularExpressions;
+
+var stacks = new List<Stack<char>>() {
+    new Stack<char>(new[] { 'R', 'P', 'C', 'D', 'B', 'G' }),
+    new Stack<char>(new[] { 'H', 'V', 'G' }),
+    new Stack<char>(new[] { 'N', 'S', 'Q', 'D', 'J', 'P', 'M' }),
+    new Stack<char>(new[] { 'P', 'S', 'L', 'G', 'D', 'C', 'N', 'M' }),
+    new Stack<char>(new[] { 'J', 'B', 'N', 'C', 'P', 'F', 'L', 'S' }),
+    new Stack<char>(new[] { 'Q', 'B', 'D', 'Z', 'V', 'G', 'T', 'S' }),
+    new Stack<char>(new[] { 'B', 'Z', 'M', 'H', 'F', 'T', 'Q' }),
+    new Stack<char>(new[] { 'C', 'M', 'D', 'B', 'F' }),
+    new Stack<char>(new[] { 'F', 'C', 'Q', 'G' }),
+};
+
+var regex = new Regex("move ([0-9]*) from ([0-9]*) to ([0-9*])");
+
+using (var sr = new StreamReader("input.txt"))
+{
+    string line;
+
+    for (int i = 0; i <= 9; i++)
+    {
+        sr.ReadLine();
+    }
+
+    while ((line = sr.ReadLine()) != null)
+    {
+        var regexCaptures = regex.Match(line).Groups.Values;
+        var quantity = int.Parse(regexCaptures.ElementAt(1).Value);
+        var from = int.Parse(regexCaptures.ElementAt(2).Value) - 1;
+        var to = int.Parse(regexCaptures.ElementAt(3).Value) - 1;
+
+        List<char> crates = new();
+        for (int i = 0; i < quantity; i++)
+        {
+            crates.Add(stacks[from].Pop());
+        }
+
+        crates.Reverse();
+
+        foreach (var crate in crates)
+        {
+            stacks[to].Push(crate);
+        }
+    }
+}
+
+Console.WriteLine($"Top of stacks: {string.Join("", stacks.Select(s => s.Count == 0 ? ' ' : s.Peek()))}");
